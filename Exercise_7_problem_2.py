fd296ffcc604f111ecbf35973ce9452f29471e70
meri809 <>
2021-05-30 18:16:26 +0000

    I added sentences to to draw a graph of monthly temperature of Helsinki.

--- Exercise_7_problem_2.py
+++ Exercise_7_problem_2.py
@@ -17,7 +17,9 @@
 # - Parse dates from the column `'DATE'` and set the dates as index in the dataframe 
 
 # YOUR CODE HERE 1 to read the data into data and parse dates
-
+import pandas as pd
+fp="data/helsinki-vantaa.csv"
+data=pd.read_csv(fp,parse_dates=['DATE'],index_col=['DATE'])
 # This test print should print first five rows
 print(data.head())
 
@@ -31,7 +33,7 @@ print(len(data))
 # - Store the selection in a new variable `selection`
 
 # YOUR CODE HERE 2
-
+selection=data.loc[(data.index >='1988-01-01')&(data.index <'2018-12-31')]
 # Check that the data was read in correctly:
 selection.head()
 
@@ -51,9 +53,16 @@ print("Number of rows:", len(selection))
 # 
 
 # YOUR CODE HERE 3
-
+import matplotlib.pyplot as plt
+selection=selection.sort_index()
+plt.plot(selection.index,selection['TEMP_C'],linestyle='solid',c='black',marker='o',markersize=3)
+plt.title("Helsinki-Vantaa Airport")
+plt.xlabel("Time")
+plt.ylabel("Temperature(Celcius)")
+plt.grid()
+plt.show()
 # Set output file name
-outputfp = ""
+outputfp = "temp_line_plot.png"
 
 # Save plot as image
 # YOUR CODE HERE 4
